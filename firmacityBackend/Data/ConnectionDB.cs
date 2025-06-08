e469e8370da5e7a7a92c07b7397fd1c9e0f096e1
Osvaldx <romanonahuel24@gmail.com>
2025-06-08 05:10:13 -0300

    Add products API: Controller, model, DB Connection

---
+++ firmacityBackend/Data/ConnectionDB.cs
@@ -0,0 +1,27 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace firmacityBackend.Data
+{
+    public class ConnectionDB
+    {
+        private MySqlConnection connection;
+        private string server = "localhost";
+        private string database = "pharmastock";
+        private string user = "root";
+        private string password = "rootnahuel";
+        private string connectionChain;
+
+        public ConnectionDB() {
+            this.connectionChain = $"Database={this.database}; DataSource={this.server}; User Id={this.user}; Password={this.password}";
+        }
+
+        public MySqlConnection getConnection() {
+            if (connection == null) {
+                this.connection = new MySqlConnection(this.connectionChain);
+                this.connection.Open();
+            }
+
+            return connection;
+        }
+    }
+}
---
+++ firmacityBackend/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace firmacityBackend.Models
+{
+    public class Product
+    {
+        public int Id { get; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+
+        public Product(int id, string name, int quantity, int price)    
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Quantity = quantity;
+            this.Price = price;
+        }
+    }
+}
