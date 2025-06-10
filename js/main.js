1e3d1c1258000886025cec728f676dd6081df4d5
Osvaldx <romanonahuel24@gmail.com>
2025-06-10 01:22:40 -0300

    add function to display categories

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 /////////////////// GLOBAL VARIABLES /////////////////
 let promotionText = document.querySelector(".promotion-text");
-//////////////////////////////////////////////////////
+let categoriesUL = document.getElementById("categories");
 let promotionMessages = [
     "Hasta 6 cuotas sin interes con tarjetas BBVA",
     "Encuentra tu sucursal más cercana",
     "Con tu compra de $ 70.000 o más, ENVIO GRATIS!"
 ];
+let categoriesList = ["Especial 2x1","Dermocosmética","Belleza","Cuidado Personal","Bebés","Hogar y Alimentos","Salud y Farmacia","Medicamentos"];
+//////////////////////////////////////////////////////
 
 const promotionTextChange = () => {
     let count = 0;
@@ -19,8 +21,19 @@ const promotionTextChange = () => {
     }, 3000);
 }
 
+const addCategoriesWeb = () => {
+    let categoriesLI = ""
+    categoriesList.forEach(name => {
+        categoriesLI += `<li><button class="btns-options">${name}</button></li>`
+    });
+
+    categoriesUL.innerHTML = categoriesLI;
+}
+
+///////////////// INICIALIZADOR ////////////////
 const init = () => {
     promotionTextChange();
+    addCategoriesWeb();
 }
 
 init();
