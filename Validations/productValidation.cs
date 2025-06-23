d2e388003a9ec0766163d1c08ea8fac36903bb96
Osvaldx <romanonahuel24@gmail.com>
2025-06-23 17:59:37 -0300

    add validations

---
+++ Validations/productValidation.cs
@@ -0,0 +1,32 @@
+﻿using firmacityBackend.Models;
+
+namespace firmacityBackend.Validations
+{
+    public static class productValidation
+    {
+        public static Boolean isValidProduct(Product product, out string? error) {
+
+            if (product.Id < 0) {
+                error = "[!] The ID of the product can´t be less than 0";
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(product.Name)) {
+                error = "[!] The product name can´t be null or ''";
+                return false;
+            }
+
+            if (product.Quantity < 0) {
+                error = "[!] The quantity of the product can´t be less than 0";
+                return false;
+            }
+
+            if (product.Price < 0) {
+                error = "[!] The price of the product can´t be less than 0";
+                return false;
+            }
+            error = null;
+            return true;
+        }
+    }
+}
